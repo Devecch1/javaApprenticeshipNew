50a55da93978b16fdab3f4ed5883105b658cc2ee
Devecch1 <brunodevecchi17@gmail.com>
2024-02-05 17:14:00 -0300

    Atualização do projeto de reservas

--- check_in_out_program/src/model/entities/Reservation.java
+++ check_in_out_program/src/model/entities/Reservation.java
@@ -1,12 +1,16 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 	
 		private Integer roomNumber;
 		private Date checkIn;
 		private Date checkOut;
+		
+		private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
 			this.roomNumber = roomNumber;
@@ -29,7 +33,34 @@ public class Reservation {
 		public Date getCheckOut() {
 			return checkOut;
 		}
-
 		
+		public long duration() {
+			long diff = checkOut.getTime() - checkIn.getTime();
+			return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);		}
+
+		public void updateDates(Date checkIn, Date checkOut) throws IllegalAccessException {
+			Date now = new Date();
+			if(checkIn.before(now) || checkOut.before(now)) {
+				throw new IllegalAccessException("Reservation dates for update must be future dates");
+			}
+			if(checkOut.after(checkIn)) {
+				throw new IllegalAccessException("Check-out date must be after check-in date");
+			}
+			this.checkIn = checkIn;
+			this.checkOut = checkOut;
+		}
+		
+		@Override
+		public String toString() {
+			return "Room "
+				+ roomNumber
+				+ ", check-in: "
+				+ sdf.format(checkIn)
+				+ ", check-out"
+				+ sdf.format(checkOut)
+				+ ", "
+				+ duration()
+				+ " nights";
+		}
 		
 }
